567bef2559ff15588ea978198bc17aa02af291bd
Rogok-Serg <rogoksv@gmail.com>
2023-05-13 16:28:23 +0300

    Task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,43 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs =  {
+  formEl: document.querySelector('.form'),
+  delayEl: document.querySelector('[name="delay"]')
+}
+
+refs.formEl.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const inputValue = {
+    delay: Number(e.target.delay.value),
+    step: Number(e.target.step.value),
+    amount: Number(e.target.amount.value),
+  }
+  for (let i = 0; i < inputValue.amount; i++) {
+    createPromise(i, inputValue.delay)
+    inputValue.delay +=inputValue.step
+  }
+
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve()
+      } else {
+        reject()
+      }
+    }, delay)
+  })
+    .then(() =>{
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(() => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+  })
+  
 }
 
 
